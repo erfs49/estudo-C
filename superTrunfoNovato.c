651ce313e8d53d50de2e41b6bf13d81db51f490f
erfs49 <146878429+erfs49@users.noreply.github.com>
2025-09-28 14:39:36 -0300

    Update superTrunfoNovato.c

--- superTrunfoNovato.c
+++ superTrunfoNovato.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char estado1, estado2;
-    char codigo1[5], codigo2[5];
     char cidade1[30], cidade2[30];
     int populacao1, populacao2;
     float area1, area2, pib1, pib2;
     int pontos1, pontos2;
+    int atributo1, atributo2;
+    float valor1a, valor2a, valor1b, valor2b;
 
-    printf("Estado da carta 1: ");
-    scanf(" %c", &estado1);
-    printf("Codigo da carta 1: ");
-    scanf(" %s", codigo1);
-    printf("Nome da cidade 1: ");
+    printf("Cidade 1: ");
     scanf(" %[^\n]", cidade1);
     printf("Populacao: ");
     scanf("%d", &populacao1);
@@ -23,11 +19,7 @@ int main() {
     printf("Pontos turisticos: ");
     scanf("%d", &pontos1);
 
-    printf("\nEstado da carta 2: ");
-    scanf(" %c", &estado2);
-    printf("Codigo da carta 2: ");
-    scanf(" %s", codigo2);
-    printf("Nome da cidade 2: ");
+    printf("\nCidade 2: ");
     scanf(" %[^\n]", cidade2);
     printf("Populacao: ");
     scanf("%d", &populacao2);
@@ -38,18 +30,52 @@ int main() {
     printf("Pontos turisticos: ");
     scanf("%d", &pontos2);
 
-    printf("\nComparacao por Populacao:\n");
-    printf("Carta 1 - %s: %d habitantes\n", cidade1, populacao1);
-    printf("Carta 2 - %s: %d habitantes\n", cidade2, populacao2);
+    float densidade1 = populacao1 / area1;
+    float densidade2 = populacao2 / area2;
 
-    if (populacao1 > populacao2)
-        printf("Resultado: Carta 1 venceu\n");
-    else if (populacao2 > populacao1)
-        printf("Resultado: Carta 2 venceu\n");
-    else
-        printf("Resultado: Empate\n");
+    printf("\nEscolha o primeiro atributo:\n");
+    printf("1 - Populacao\n2 - Area\n3 - PIB\n4 - Pontos turisticos\n5 - Densidade\n");
+    scanf("%d", &atributo1);
+
+    printf("\nEscolha o segundo atributo (diferente do primeiro):\n");
+    scanf("%d", &atributo2);
+
+    if (atributo1 == atributo2) {
+        printf("Os atributos devem ser diferentes\n");
+        return 1;
+    }
+
+    switch (atributo1) {
+        case 1: valor1a = populacao1; valor2a = populacao2; break;
+        case 2: valor1a = area1; valor2a = area2; break;
+        case 3: valor1a = pib1; valor2a = pib2; break;
+        case 4: valor1a = pontos1; valor2a = pontos2; break;
+        case 5: valor1a = densidade1; valor2a = densidade2; break;
+        default: printf("Atributo 1 invalido\n"); return 1;
+    }
+
+    switch (atributo2) {
+        case 1: valor1b = populacao1; valor2b = populacao2; break;
+        case 2: valor1b = area1; valor2b = area2; break;
+        case 3: valor1b = pib1; valor2b = pib2; break;
+        case 4: valor1b = pontos1; valor2b = pontos2; break;
+        case 5: valor1b = densidade1; valor2b = densidade2; break;
+        default: printf("Atributo 2 invalido\n"); return 1;
+    }
+
+    float soma1 = valor1a + valor1b;
+    float soma2 = valor2a + valor2b;
+
+    printf("\n%s: %.2f + %.2f = %.2f\n", cidade1, valor1a, valor1b, soma1);
+    printf("%s: %.2f + %.2f = %.2f\n", cidade2, valor2a, valor2b, soma2);
+
+    printf("Resultado: %s\n", 
+        (soma1 > soma2) ? cidade1 : 
+        (soma2 > soma1) ? cidade2 : "Empate");
 
     return 0;
 }
+
+
 
 
