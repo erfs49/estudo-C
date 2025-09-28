07abe8f781fed58710dd5f3cdd832f7f2a9419e6
erfs49 <146878429+erfs49@users.noreply.github.com>
2025-09-28 17:41:28 -0300

    Create PecaXadrezNovato.c

---
+++ PecaXadrezNovato.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+int main(){
+  int movimentoTorre = 5 , 
+      movimentobispo = 5 ,
+      movimentoRainha = 8;
+
+  printf("movimento torre:\n");
+
+  for(int i = 0; i; i < movimentoTorre;i++){
+  printf("%d-direita",i);
+  }
+  return 0;
+}
+
