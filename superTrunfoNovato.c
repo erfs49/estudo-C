fd4096bb10f941753aac5843d3a90b95483b6038
erfs49 <146878429+erfs49@users.noreply.github.com>
2025-09-28 14:38:44 -0300

    Update superTrunfoNovato.c

--- superTrunfoNovato.c
+++ superTrunfoNovato.c
@@ -8,50 +8,48 @@ int main() {
     float area1, area2, pib1, pib2;
     int pontos1, pontos2;
 
-    // Entrada carta 1
     printf("Estado da carta 1: ");
     scanf(" %c", &estado1);
-    printf("Código da carta 1: ");
+    printf("Codigo da carta 1: ");
     scanf(" %s", codigo1);
     printf("Nome da cidade 1: ");
     scanf(" %[^\n]", cidade1);
-    printf("População: ");
+    printf("Populacao: ");
     scanf("%d", &populacao1);
-    printf("Área: ");
+    printf("Area: ");
     scanf("%f", &area1);
     printf("PIB: ");
     scanf("%f", &pib1);
-    printf("Pontos turísticos: ");
+    printf("Pontos turisticos: ");
     scanf("%d", &pontos1);
 
-    // Entrada carta 2
     printf("\nEstado da carta 2: ");
     scanf(" %c", &estado2);
-    printf("Código da carta 2: ");
+    printf("Codigo da carta 2: ");
     scanf(" %s", codigo2);
     printf("Nome da cidade 2: ");
     scanf(" %[^\n]", cidade2);
-    printf("População: ");
+    printf("Populacao: ");
     scanf("%d", &populacao2);
-    printf("Área: ");
+    printf("Area: ");
     scanf("%f", &area2);
     printf("PIB: ");
     scanf("%f", &pib2);
-    printf("Pontos turísticos: ");
+    printf("Pontos turisticos: ");
     scanf("%d", &pontos2);
 
-    // Comparação por população
-    printf("\nComparação por População:\n");
-    printf("%s: %d habitantes\n", cidade1, populacao1);
-    printf("%s: %d habitantes\n", cidade2, populacao2);
+    printf("\nComparacao por Populacao:\n");
+    printf("Carta 1 - %s: %d habitantes\n", cidade1, populacao1);
+    printf("Carta 2 - %s: %d habitantes\n", cidade2, populacao2);
 
     if (populacao1 > populacao2)
-        printf("Resultado: %s venceu!\n", cidade1);
+        printf("Resultado: Carta 1 venceu\n");
     else if (populacao2 > populacao1)
-        printf("Resultado: %s venceu!\n", cidade2);
+        printf("Resultado: Carta 2 venceu\n");
     else
-        printf("Resultado: Empate!\n");
+        printf("Resultado: Empate\n");
 
     return 0;
 }
+
 
